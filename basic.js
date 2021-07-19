2e3bb002a3d5c2b93ef63ce6be388f97f2d279a4
Nipun Garg <garg2002nipun@gmail.com>
2021-07-19 18:24:00 +0530

    Practice

---
+++ basic.js
@@ -0,0 +1,20 @@
+document.write("Hello World! I'm Nipun Garg <br/>");
+document.write("Hello World! I'm Nipun Garg 2 <br>");
+document.write("Hello World! I'm Nipun Garg 3<br>");
+
+
+// for next giving some space write \n
+
+
+// document.write(10 + 5, '\n');
+// document.write(10 + 5, '\n');
+// document.write(10 + 5, '\n');
+// document.write(10 + 5, '\n');
+
+//  for next line use <br/>
+
+
+// document.write(10 + 5, '<br/>');
+// document.write(10 + 5, '<br/>');
+// document.write(10 + 5, '<br/>');
+// document.write(10 + 5, '<br/>');
